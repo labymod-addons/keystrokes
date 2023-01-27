dfc0fe380c226ab9af2f8f2125ae78948fb64347
Nico <jumpingpxl@gmail.com>
2023-01-27 15:35:18 +0100

    make enabled a setting

--- core/src/main/java/net/labymod/addons/keystrokes/KeyStrokesConfiguration.java
+++ core/src/main/java/net/labymod/addons/keystrokes/KeyStrokesConfiguration.java
@@ -17,12 +17,14 @@
 package net.labymod.addons.keystrokes;
 
 import net.labymod.api.addon.AddonConfig;
+import net.labymod.api.client.gui.screen.widget.widgets.input.SwitchWidget.SwitchSetting;
 import net.labymod.api.configuration.loader.annotation.ConfigName;
 import net.labymod.api.configuration.loader.property.ConfigProperty;
 
 @ConfigName("settings")
 public class KeyStrokesConfiguration extends AddonConfig {
 
+  @SwitchSetting
   private final ConfigProperty<Boolean> enabled = new ConfigProperty<>(true);
 
   @Override
